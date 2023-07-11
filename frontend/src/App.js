736ea1a51068f4ff57177cc8ed3eae036a6ec7ab
Zeb Gambill <gambillzc@gmail.com>
2023-07-11 14:42:11 -0400

    backend added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
-  return <p>Welcome to ProShop</p>
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App
